d9a91e6b31ff48b1613429a4e271e82f7e12f88f
weber <wwang035@fiu.edu>
2016-03-04 19:19:39 -0500

    Be able to communicate with server through web service

--- hmtsm/app/src/main/java/edu/fiu/hmts_cu/LoginActivity.java
+++ hmtsm/app/src/main/java/edu/fiu/hmts_cu/LoginActivity.java
@@ -1,20 +1,64 @@
 package edu.fiu.hmts_cu;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.concurrent.ExecutionException;
+
+import edu.fiu.hmts_cu.service.HttpService;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private ProgressBar load;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        load = (ProgressBar)LoginActivity.this.findViewById(R.id.load);
     }
 
-    public void openMenuActivity(View view) {
-        Intent menuIntent = new Intent(this, MenuActivity.class);
-        startActivity(menuIntent);
+
+    class service extends AsyncTask<JSONObject, Void, String> {
+        @Override
+        protected void onPreExecute() {
+            load.setIndeterminate(true);
+            load.setVisibility(View.VISIBLE);
+        }
+
+        protected String doInBackground(JSONObject... params) {
+            return HttpService.requestService(params[0]);
+        }
+
+        protected void onPostExecute(Void result) {
+            load.setVisibility(View.GONE);
+        }
+    }
+
+    public void openMenuActivity(View view) throws Exception {
+        JSONObject input = new JSONObject();
+        EditText usernm = (EditText)LoginActivity.this.findViewById(R.id.usernm);
+        EditText passwd = (EditText)LoginActivity.this.findViewById(R.id.passwd);
+        input.put("username", usernm.getText());
+        input.put("password", passwd.getText());
+        String res = new service().execute(input).get();
+
+        if (!"Not Found".equals(res)) {
+            JSONObject resJson = new JSONObject(res);
+            Intent menuIntent = new Intent(this, MenuActivity.class);
+            menuIntent.putExtra("UserId", resJson.getString("userId"));
+            menuIntent.putExtra("Phone", resJson.getString("phone"));
+            startActivity(menuIntent);
+        }
     }
 }

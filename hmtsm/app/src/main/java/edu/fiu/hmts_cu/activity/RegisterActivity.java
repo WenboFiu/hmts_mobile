60b1daf3547efb5eb20b325a76cc83881b33d1aa
weber <wwang035@fiu.edu>
2016-04-08 06:04:35 -0400

    Update

--- hmtsm/app/src/main/java/edu/fiu/hmts_cu/activity/RegisterActivity.java
+++ hmtsm/app/src/main/java/edu/fiu/hmts_cu/activity/RegisterActivity.java
@@ -1,12 +1,16 @@
 package edu.fiu.hmts_cu.activity;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -25,6 +29,9 @@ import edu.fiu.hmts_cu.controller.MobileController;
  * Class for RegisterActivity
  */
 public class RegisterActivity extends Activity {
+
+    List<String> quesitems;
+    List<String> quesIditems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,41 +53,50 @@ public class RegisterActivity extends Activity {
         });
 
         Spinner question = (Spinner)findViewById(R.id.question);
-        List<String> items = getQuestionItems();
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,items);
+        getQuestionItems();
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,quesitems);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         question.setAdapter(adapter);
     }
 
-    private List<String> getQuestionItems() {
-        List<String> items = new ArrayList<String>();
+    private void getQuestionItems() {
+        quesIditems = new ArrayList<>();
+        quesitems = new ArrayList<>();
         try {
-            JSONArray questions = new Service().execute().get();
+            JSONObject param = new JSONObject();
+            param.put("service", "getquestions");
+            JSONArray questions = new Service().execute(param).get().getJSONArray("data");
             for (int i = 0; i < questions.length(); i++) {
-                items.add(questions.getJSONObject(i).get("content").toString());
+                quesIditems.add(questions.getJSONObject(i).get("secQuestionId").toString());
+                quesitems.add(questions.getJSONObject(i).get("content").toString());
             }
-            return items;
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        } catch (JSONException e) {
+        } catch (InterruptedException | ExecutionException | JSONException e) {
             e.printStackTrace();
         }
-        return items;
     }
 
     /**
      * Class for async task.
      */
-    class Service extends AsyncTask<Void, Void, JSONArray> {
+    class Service extends AsyncTask<JSONObject, Void, JSONObject> {
         @Override
         protected void onPreExecute() {
 
         }
 
-        protected JSONArray doInBackground(Void... params) {
-            return MobileController.getQuestions();
+        protected JSONObject doInBackground(JSONObject... params) {
+            try {
+                if (params[0].get("service").equals("register")){
+                    params[0].remove("service");
+                    return MobileController.register(params[0]);
+                }
+                else if (params[0].get("service").equals("getquestions")){
+                    return MobileController.getQuestions();
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return new JSONObject();
         }
 
         protected void onPostExecute() {
@@ -92,7 +108,52 @@ public class RegisterActivity extends Activity {
      * Create a user.
      */
     public void register(View view) throws Exception{
-        JSONObject regData = new JSONObject();
+        if (checkFields()) return;
 
+        JSONObject regData = new JSONObject();
+        regData.put("username", ((EditText)findViewById(R.id.email)).getText().toString());
+        regData.put("password", ((EditText)findViewById(R.id.passwd)).getText().toString());
+        regData.put("firstName", ((EditText)findViewById(R.id.firstnm)).getText().toString());
+        regData.put("lastName", ((EditText)findViewById(R.id.lastnm)).getText().toString());
+        regData.put("phone", ((EditText)findViewById(R.id.phone)).getText().toString());
+        regData.put("role", "4");
+        regData.put("secQuestionId", quesIditems.get(((Spinner) findViewById(R.id.question)).getSelectedItemPosition()));
+        regData.put("secAnswer", ((EditText)findViewById(R.id.answer)).getText().toString());
+        regData.put("service", "register");
+
+        JSONObject regRes = new Service().execute(regData).get();
+        if ("successful".equals(regRes.get("result").toString())
+                && !"-1".equals(regRes.getJSONObject("data").get("userId").toString())) {
+            Intent menuIntent = new Intent(this, MenuActivity.class);
+            menuIntent.putExtra("UserId", regRes.getJSONObject("data").getString("userId"));
+            menuIntent.putExtra("Phone", regRes.getJSONObject("data").getString("phone"));
+            startActivity(menuIntent);
+            this.finish();
+        }
+    }
+
+    private boolean checkFields() {
+        if ("".equals(((EditText)findViewById(R.id.email)).getText().toString())
+                || "".equals(((EditText)findViewById(R.id.passwd)).getText().toString())
+                || "".equals(((EditText)findViewById(R.id.firstnm)).getText().toString())
+                || "".equals(((EditText)findViewById(R.id.lastnm)).getText().toString())
+                || "".equals(((EditText)findViewById(R.id.phone)).getText().toString())
+                || ((Spinner)findViewById(R.id.question)).getSelectedItemPosition() < 0
+                || "".equals(((EditText)findViewById(R.id.answer)).getText().toString())){
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Warning");
+            builder.setMessage("Registration information is incomplete.");
+            builder.setCancelable(true);
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.cancel();
+                }
+            });
+            AlertDialog alertDialog = builder.create();
+            alertDialog.show();
+            return true;
+        }
+        return false;
     }
 }
